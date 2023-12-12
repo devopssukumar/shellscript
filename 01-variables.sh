e7ee7e2a769866e4f6347ebd9765b3044845c23b
sukumar <sukumarg.7310@gmail.com>
2023-12-12 21:14:14 +0530

    shellscript

--- 01-variables.sh
+++ 01-variables.sh
@@ -7,6 +7,6 @@ DATE=$(date)
 echo "$PERSON1: Hi $PERSON2, Good morning"
 echo "$PERSON2: Hello $PERSON1, very Good morning"
 echo "$PERSON1: How are you $PERSON2 ?"
-echo "$PERSON2: I am good $PERSON1, Thank you"
+echo "$PERSON2: I am good $PERSON1, Thank you."
 
-echo "This script is generated on $(DATE)"
+echo "This script is generated on $DATE"
