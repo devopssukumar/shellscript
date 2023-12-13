157eac660fd4680caffbf2910a56e0bb87304c77
sukumar <sukumarg.7310@gmail.com>
2023-12-13 22:11:16 +0530

    installing packages

--- install-packages.sh
+++ install-packages.sh
@@ -3,27 +3,31 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="shell-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR:: Please run this script with Root user"
+    echo -e "$R ERROR:: Please run this script with Root user $N"
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ... FAILED"
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 yum install mysql -y &>> $LOGFILE
 
-VALIDATE $? "Installing Mysql"
+VALIDATE $? "Installing $Y Mysql $N"
 
 yum install git -y &>> $LOGFILE
 
-VALIDATE $? "Installing Git"
+VALIDATE $? "Installing $Y Git $N"
