9d350565690f47ab90d9ae853a69c7bf34d299d9
sukumar <sukumarg.7310@gmail.com>
2023-12-13 21:54:46 +0530

    installing packages

---
+++ install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/temp/shell-$TIMESTAMP.log"
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with Root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing Mysql"
+
+yum install git -y $>> $LOGFILE
+
+VALIDATE $? "Installing Git"
