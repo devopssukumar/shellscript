db53b9edc6f114cba77dc9713bc1e0a954d813aa
sukumar <sukumarg.7310@gmail.com>
2024-01-19 09:22:51 +0530

    shell-script files renamed

---
+++ 07-delete-old-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/shell-script.logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ] # -d is dir exists| ! is opposite=if dir not exists
+then
+    echo -e "$R source directory: $SOURCE_DIR does not exists. $N"
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +10 -name "*.log")
+
+while ifs= read -r line # read -r line=Reads line by line | ifs=internal field seperator
+do
+    echo "Deleting file: $line"
+done <<< $FILES_TO_DELETE
