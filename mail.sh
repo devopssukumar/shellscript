e606a246104757dfe6799f856419f65ec2b93440
sukumar <sukumarg.7310@gmail.com>
2024-01-24 19:27:43 +0530

    roboshop-shell disk usage

--- mail.sh
+++ mail.sh
@@ -8,4 +8,4 @@ SUBJECT=$5
 
 OUTPUT_BODY=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
 
-echo "$OUTPUT_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+echo "$OUTPUT_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
