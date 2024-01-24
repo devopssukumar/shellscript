9170a91fff2d2df78106fee62cce3b2dc3a1a6f4
sukumar <sukumarg.7310@gmail.com>
2024-01-24 19:41:49 +0530

    roboshop-shell disk usage

--- mail.sh
+++ mail.sh
@@ -3,10 +3,10 @@
 TO_TEAM=$1
 ALERT_TYPE=$2
 BODY=$3
-ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e "s/[]\/$*.^[]/\\&/g");
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
 
-OUTPUT_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
-echo "$OUTPUT_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
